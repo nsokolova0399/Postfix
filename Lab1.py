93334bfc57fc6f8aab72556b0ed74026bfe8aa44
Anastasia Sokolova <nsokolova0399@mail.ru>
2022-02-28 17:42:41 +0300

    1

---
+++ Lab1.py
@@ -0,0 +1,136 @@
+from pythonds.basic.stack import Stack
+
+def infix_to_postfix(infix):
+    with_enter = ''
+    check_altern = True  # операнд
+    flag = False
+    if infix[0] == '-':
+        flag = True
+    elif infix[0] == '+':
+        infix = infix[1:]
+    for i in range(len(infix)):
+        print(infix[i])
+        if infix[i] != ' ':
+            if infix[i] == '.' or flag:
+                if flag:
+                    with_enter += infix[i]+' '
+                else:
+                    with_enter += infix[i]
+                flag = False
+            else:
+                if infix[i] != '(' and infix[i] != ')':
+                    # Проверить что происходит чередование символов
+                    if check_altern:
+                        # Надо считать до момент пока не встретили пробел
+                        if infix[i].lower() in "abcdefghijklmnopqrstuvwxyz" or infix[i].isdigit():
+                            if i != len(infix) - 1:
+                                if not infix[i + 1].isdigit() and infix[i + 1] != '.':
+                                    check_altern = False
+                        else:
+                            raise Exception("Некорректная запись")
+                    else:
+                        if infix[i] == '+' or infix[i] == '-' or infix[i] == '*' or infix[i] == '/':
+                            check_altern = True
+                        else:
+                            raise Exception("Некорректная запись")
+                    # if not check_num:
+                    # проверим нужно ли добавлять пробел
+                    if i != len(infix) - 1 or infix[-1] == ')' or infix[-1] == '(':
+                        if (infix[i + 1].isdigit() or infix[i + 1] == '.') and infix[i] != '+' and infix[i] != '*' and \
+                                infix[i] != '-' and infix[i] != '/':
+                            with_enter += infix[i]
+                        else:
+                            with_enter += infix[i] + ' '
+                    else:
+                        with_enter += infix[i] + ' '
+                else:
+                    with_enter += infix[i] + ' '
+
+    infix = with_enter
+    priority_operation = {}
+    priority_operation["*"] = 3
+    priority_operation["/"] = 3
+    priority_operation["+"] = 2
+    priority_operation["-"] = 2
+    priority_operation["("] = 1
+    op_stack = Stack()
+    postfix_list = []
+    token_list = infix.split()
+
+    for token in token_list:
+        if token.lower() in "abcdefghijklmnopqrstuvwxyz" or token.isdigit():
+            postfix_list.append(token)
+        elif token == '(':
+            op_stack.push(token)
+        elif token == ')':
+            top_token = op_stack.pop()
+            while top_token != '(':
+                postfix_list.append(top_token)
+                top_token = op_stack.pop()
+        else:
+            while (not op_stack.isEmpty()) and \
+                    (priority_operation[op_stack.peek()] >= priority_operation[token]):
+                postfix_list.append(op_stack.pop())
+            op_stack.push(token)
+
+    while not op_stack.isEmpty():
+        postfix_list.append(op_stack.pop())
+
+    if '(' in postfix_list or ')' in postfix_list:
+        raise Exception("Некорректная запись")
+    return " ".join(postfix_list)
+
+
+def postfix_eval(postfix_expr):
+    operand_stack = Stack()
+    token_list = postfix_expr.split()
+    for token in token_list:
+        if token.lower() in "abcdefghijklmnopqrstuvwxyz" or token.isdigit():
+            if token.lower() in "abcdefghijklmnopqrstuvwxyz":
+                num = float(input(token + " = "))
+                operand_stack.push(num)
+            else:
+                operand_stack.push(float(token))
+        else:
+            operand2 = operand_stack.pop()
+            if operand_stack.isEmpty() and token == "-":
+                operand1 = '~'
+            else:
+                operand1 = operand_stack.pop()
+            result = math_compute(token, operand1, operand2)
+            operand_stack.push(result)
+    return operand_stack.pop()
+
+
+def math_compute(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    elif op1 == "~":
+        return -op2
+    else:
+        return op1 - op2
+
+
+while True:
+    print("-" * 20)
+    try:
+        infix = input("Введите выражение: ")
+        postfix = infix_to_postfix(infix)
+        if not postfix and ('+' not in postfix and '-' not in postfix and '*' not in postfix and '/' not in postfix):
+            raise Exception
+        print("Постфиксная запись: {}".format(postfix))
+        try:
+            answer = postfix_eval(postfix)
+            print("Результат = {}".format(answer))
+        except ZeroDivisionError:
+            print("Деление на ноль запрещено!")
+        except ValueError:
+            print("Ожидалось вещественное число.")
+        except TypeError:
+            print("Не тот тип записи")
+    except Exception:
+        print("Некорректная запись")
---
+++ Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,310 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <valarray>
+#include <map>
+#include <list>
+#include <stack>
+#include <regex>
+
+using namespace std;
+
+ifstream f_in;
+ofstream f_out;
+
+string StrToPostfix(string str) {
+	stack <const char*> StrStack;
+	map<char, int>  priority_operation;
+	priority_operation['*'] = 3;
+	priority_operation['/'] = 3;
+	priority_operation['+'] = 2;
+	priority_operation['-'] = 2;
+	priority_operation['('] = 1;
+
+	vector <const char*> ListPostfix;
+	map<int, string> Infix;
+	vector<string> vec1;
+	vector<const char*> vec2;
+	string StrPostfix;
+	int k = 0;
+	int m = 0;
+
+	
+	while (k < str.size()) {
+		/*string str1;
+		m = k;
+		while (isdigit(str[k]) != 0) {
+			str1 += str[k];
+			k++;
+		}
+		if (isdigit(str[m]) != 0) {
+			Infix[m] = str1;
+		}
+		else {*/
+			Infix[k] += str[k];
+			k++;
+		//}
+	}
+
+	for (int i = 0; i < Infix.size(); i++) {
+		if (Infix[i] != "") {
+			vec1.push_back(Infix[i]);
+		}
+	}
+	
+
+	for (int i = 0; i < vec1.size(); i++) {
+		vec2.resize(vec1.size(), nullptr);
+
+		transform(begin(vec1), end(vec1), begin(vec2), [&](const string& str)
+		{
+			return str.c_str();
+		});
+	}
+ 
+	for (int i = 0; i < vec2.size(); i++) {
+		
+		if (isalpha(*vec2[i])|| isdigit(*vec2[i]) != 0 ) {
+			ListPostfix.push_back(vec2[i]);
+		}
+		else 
+			if (*vec2[i] == '(') {
+			StrStack.push(vec2[i]);
+		}
+		else if (*vec2[i] == ')') {
+			
+			while(*StrStack.top() != '(') {
+				const char* current = StrStack.top();
+				StrStack.pop();
+				if (priority_operation[*current]) {
+					ListPostfix.push_back(current);
+				}
+				if (StrStack.size() == 0) {
+					f_out << "Ошибка, неверно поставлен разделитель или не согласованы скобки";
+					throw ("Ошибка, неверно поставлен разделитель или не согласованы скобки");
+				}
+			}
+			StrStack.pop();
+		}
+		else if (priority_operation[*vec2[i]]) {
+			while (StrStack.size() != 0 && priority_operation[*StrStack.top()] >= priority_operation[*vec2[i]]) {
+				ListPostfix.push_back(StrStack.top());
+				StrStack.pop();
+			}
+			StrStack.push(vec2[i]);
+		}
+	}
+	while (StrStack.size() != 0) {
+		if (StrStack.top() != "(" && vec2.size()>2) {
+				ListPostfix.push_back(StrStack.top());
+				StrStack.pop();
+		}
+		else {
+			f_out << "Ошибка, неверно поставлен разделитель или не согласованы скобки\n";
+			throw ("Ошибка, неверно поставлен разделитель или не согласованы скобки\n");
+		}
+	}
+
+	for (const char* StrBuf: ListPostfix) {
+		StrPostfix += StrBuf;
+	}
+	
+	return StrPostfix;
+}
+
+map <string, double> FindOperation(string str) {
+	map<string, double> operation;
+	string StrBuf;
+	vector <const char*> ListPostfix;
+	map<int, string> Infix;
+	vector<string> vec1;
+	vector<const char*> vec2;
+	string StrPostfix;
+	int k = 0;
+	int m = 0;
+
+	//строку распределяем по символам
+	while (k < str.size()) {
+		/*string str1;
+		m = k;
+		while (isdigit(str[k]) != 0) {
+			str1 += str[k];
+			k++;
+		}
+		if (isdigit(str[m]) != 0) {
+			Infix[m] = str1;
+		}
+		else {*/
+		Infix[k] += str[k];
+		k++;
+		//}
+	}
+
+	for (int i = 0; i < Infix.size(); i++) {
+		if (Infix[i] != "") {
+			vec1.push_back(Infix[i]);
+		}
+	}
+	for (int i = 0; i < vec1.size(); i++) {
+		vec2.resize(vec1.size(), nullptr);
+
+		transform(begin(vec1), end(vec1), begin(vec2), [&](const string& str)
+		{
+			return str.c_str();
+		});
+	}
+
+
+	for (int i = 0; i < vec2.size(); i++) {
+		if (isalpha(*vec2[i])) {
+			if (!operation[vec2[i]]) {
+				cout << vec2[i]<<"=";
+				cin >> StrBuf;
+				f_out << vec2[i] << " = " << StrBuf<<endl;
+				double buf = stod(StrBuf);
+				operation[vec2[i]] = buf;
+			}
+		}
+		else if (isdigit(*vec2[i])) {
+			double buf = stod(vec2[i]);
+			operation[vec2[i]] = buf;
+		}
+	}
+	return operation;
+}
+
+
+double Operator(string Operand, double operand1, double operand2) {
+	double result;
+	if (Operand == "*") result = operand1 * operand2;
+	else if (Operand == "/") {
+		if (operand2 == 0) {
+			f_out << "Деление на 0 невозможно";
+			throw "Деление на 0 невозможно";
+		}
+		result = operand1 / operand2;
+	}
+	else if (Operand == "+") result = operand1 + operand2;
+	else if (Operand == "-") result = operand1 - operand2;
+	else { 
+		f_out << "Ошибка, неизвестный оператор";
+		throw "Ошибка, неизвестный оператор"; }
+	return result;
+}
+
+double Calculation(map <string, double>  operand, string str) {
+	double result;
+	stack <double> StrStack;
+	vector <const char*> ListPostfix;
+	map<int, string> Infix;
+	vector<string> vec1;
+	vector<const char*> vec2;
+	string StrPostfix;
+	int k = 0;
+	int m = 0;
+
+	//строку распределяем по символам
+	while (k < str.size()) {
+		/*string str1;
+		m = k;
+		while (isdigit(str[k]) != 0) {
+			str1 += str[k];
+			k++;
+		}
+		if (isdigit(str[m]) != 0) {
+			Infix[m] = str1;
+		}
+		else {*/
+		Infix[k] += str[k];
+		k++;
+		//}
+	}
+
+	for (int i = 0; i < Infix.size(); i++) {
+		if (Infix[i] != "") {
+			vec1.push_back(Infix[i]);
+		}
+	}
+	for (int i = 0; i < vec1.size(); i++) {
+		vec2.resize(vec1.size(), nullptr);
+
+		transform(begin(vec1), end(vec1), begin(vec2), [&](const string& str)
+		{
+			return str.c_str();
+		});
+	}
+
+
+	for (int i = 0; i < vec2.size(); i++) {
+		if (operand[vec2[i]]) {
+			StrStack.push(operand[vec2[i]]);
+		}
+		else {
+			double operand2 = StrStack.top();
+			StrStack.pop();
+			if (StrStack.size() != 0) {
+				double operand1 = StrStack.top();
+				StrStack.pop();
+				result = Operator(vec2[i], operand1, operand2);
+			}else if (*vec2[i] == '-') {
+				result = -operand2;
+			}
+			else if (*vec2[i] == '+') {
+				result = operand2;
+			}
+			else {
+				throw "Ошибка, недостаточно операндов.";
+			}
+			StrStack.push(result);
+		}
+	}
+	return result;
+}
+
+
+int main()
+{
+	try
+	{
+		f_in.open("input.txt");
+		try
+		{
+			f_out.open("output.txt");
+			string str;
+			while (getline(f_in, str)) {
+				if (str == "" || str == "\n")  return 0;
+				else {
+					try {
+						string postfix = StrToPostfix(str);
+						f_out << "Исходное выражение: " <<str<< '\n';
+						f_out << "Выражение в  постфиксной форме: " << postfix << '\n';
+						map <string, double> operation = FindOperation(str);
+						double ResultPostfix = Calculation(operation, postfix);
+						f_out << "Результат вычислений в постфиксной форме: " << ResultPostfix << '\n';
+						
+					}
+					catch (const char* exception) {
+						cerr << "Error: " << exception << '\n';
+					}
+				}
+
+			}
+		}
+		catch (std::ofstream::failure &writeErr) {
+			cout << "Unable to open the output file\n";
+		}		
+
+	}
+	catch (std::ifstream::failure &readErr) {
+		cout << "The input data file was not opened\n";
+	}
+}
+
+
+
